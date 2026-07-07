5d47cf417c87657b479bc877418a7d86baf20f21
Pratik Patel <pratiikpatel.001@gmail.com>
2026-07-07 15:35:44 +0545

    feat:ServicesHero

--- src/components/sections/services/utils.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-// import { bgColors } from "src/constants/hero.ts";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
