557546296be5657949b25cfd74778f127a00503f
Pratik Patel <pratiikpatel.001@gmail.com>
2026-07-12 15:17:30 +0545

    ServicesPage Refactor4

--- src/components/sections/services/hero/Hero.tsx
+++ src/components/sections/services/hero/Hero.tsx
@@ -18,10 +18,10 @@ export default function Hero() {
     >
       <Section
         className="
-         
-        bg-soft
           relative
           overflow-hidden
+
+          bg-[linear-gradient(180deg,#FFFFFF_0%,#F8FBFE_100%)]
 
           pt-20
           pb-16
@@ -33,23 +33,95 @@ export default function Hero() {
           lg:pb-24
         "
       >
+        {/* ================================================= */}
+        {/* Background Glow */}
+        {/* ================================================= */}
+
+        <div
+          className="
+            pointer-events-none
+            absolute
+
+            -left-40
+            top-0
+
+            h-[36rem]
+            w-[36rem]
+
+            rounded-full
+
+            bg-[#2374B6]/5
+
+            blur-[180px]
+          "
+        />
+
+        <div
+          className="
+            pointer-events-none
+            absolute
+
+            right-[-12rem]
+            top-20
+
+            h-[32rem]
+            w-[32rem]
+
+            rounded-full
+
+            bg-[#113358]/5
+
+            blur-[170px]
+          "
+        />
+
+        <div
+          className="
+            pointer-events-none
+            absolute
+
+            left-1/2
+            bottom-[-14rem]
+
+            h-[34rem]
+            w-[34rem]
+
+            -translate-x-1/2
+
+            rounded-full
+
+            bg-[#67B7F7]/5
+
+            blur-[190px]
+          "
+        />
+
         <Container size="xl">
           <div
             className="
+              relative
+              z-10
+
               grid
               items-center
 
-              gap-12
+              gap-14
 
-              lg:grid-cols-2
-              lg:gap-16
-
-              xl:gap-20
+              lg:grid-cols-12
+              lg:gap-20
             "
           >
-            <HeroContent />
+            {/* Content */}
 
-            <HeroImage />
+            <div className="lg:col-span-5">
+              <HeroContent />
+            </div>
+
+            {/* Dashboard */}
+
+            <div className="lg:col-span-7">
+              <HeroImage />
+            </div>
           </div>
         </Container>
       </Section>
